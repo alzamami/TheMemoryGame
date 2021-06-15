3b0c513510540aa7789b9270854149047f7d4408
Mohammad Alzamami <alzamami981@gmail.com>
2021-06-15 19:53:19 +0300

    started the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { HeaderStyled, SubheadingStyled } from "./styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderStyled>The Memory Game</HeaderStyled>
+      <SubheadingStyled>
+        This is a game to test your ability of Memorizing
+      </SubheadingStyled>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderStyled = styled.h1`
+  font-size: 40px;
+  text-align: center;
+`;
+export const SubheadingStyled = styled.p`
+  font-size: 20px;
+  text-align: center;
+`;
